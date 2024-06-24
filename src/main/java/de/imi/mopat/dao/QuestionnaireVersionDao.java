19ee3fa0e6a649a9fd7aa3538b42f436b438d761
schluebbo <lueder.jungjohann@uni-muenster.de>
2024-06-24 09:40:06 +0200

    #20: Created QuestionnaireVersionDao interface for handling CRUD operations on questionnaire versions

---
+++ src/main/java/de/imi/mopat/dao/QuestionnaireVersionDao.java
@@ -0,0 +1,8 @@
+package de.imi.mopat.dao;
+
+import de.imi.mopat.model.Answer;
+import de.imi.mopat.model.QuestionnaireVersion;
+
+public interface QuestionnaireVersionDao extends MoPatDao<QuestionnaireVersion> {
+
+}
