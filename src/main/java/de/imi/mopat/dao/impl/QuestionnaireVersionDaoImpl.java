5a32080742cf8f7a411d92c6504032322ae6fd58
schluebbo <lueder.jungjohann@uni-muenster.de>
2024-06-24 09:40:37 +0200

    #20: Implemented QuestionnaireVersionDao interface in QuestionnaireVersionDaoImpl to provide database interaction logic for questionnaire versions.

---
+++ src/main/java/de/imi/mopat/dao/impl/QuestionnaireVersionDaoImpl.java
@@ -0,0 +1,10 @@
+package de.imi.mopat.dao.impl;
+
+import de.imi.mopat.dao.QuestionnaireVersionDao;
+import de.imi.mopat.model.QuestionnaireVersion;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestionnaireVersionDaoImpl extends MoPatDaoImpl<QuestionnaireVersion> implements QuestionnaireVersionDao {
+
+}
