934f7986499c32aee5bd2ce64f41a243f97f2e20
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-07-02 15:29:48 +0200

    Added endpoints to handle pin login and new user fields

--- src/main/java/de/imi/mopat/controller/UserController.java
+++ src/main/java/de/imi/mopat/controller/UserController.java
@@ -1,43 +1,44 @@
 package de.imi.mopat.controller;
 
 import de.imi.mopat.auth.MoPatActiveDirectoryLdapAuthenticationProvider;
+import de.imi.mopat.auth.PinAuthorizationService;
+import de.imi.mopat.dao.ClinicDao;
+import de.imi.mopat.dao.ConfigurationDao;
 import de.imi.mopat.dao.user.AclClassDao;
 import de.imi.mopat.dao.user.AclEntryDao;
 import de.imi.mopat.dao.user.AclObjectIdentityDao;
-import de.imi.mopat.dao.ClinicDao;
-import de.imi.mopat.dao.ConfigurationDao;
 import de.imi.mopat.dao.user.ForgotPasswordTokenDao;
 import de.imi.mopat.dao.user.InvitationDao;
+import de.imi.mopat.dao.user.PinAuthorizationDao;
 import de.imi.mopat.dao.user.UserDao;
 import de.imi.mopat.helper.controller.ApplicationMailer;
 import de.imi.mopat.helper.controller.CacheService;
 import de.imi.mopat.helper.controller.Constants;
+import de.imi.mopat.model.Clinic;
+import de.imi.mopat.model.enumeration.PermissionType;
 import de.imi.mopat.model.user.AclEntry;
 import de.imi.mopat.model.user.Authority;
-import de.imi.mopat.model.Clinic;
 import de.imi.mopat.model.user.ForgotPasswordToken;
 import de.imi.mopat.model.user.Invitation;
-import de.imi.mopat.model.enumeration.PermissionType;
+import de.imi.mopat.model.user.PinAuthorization;
 import de.imi.mopat.model.user.User;
 import de.imi.mopat.model.user.UserRole;
 import de.imi.mopat.validator.UserValidator;
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Set;
-
-import org.slf4j.Logger;
-
 import java.util.List;
 import java.util.Locale;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import java.util.Set;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.MailException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -58,9 +59,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("hideProfile")
 public class UserController {
 
-    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(UserController.class);
     public static final String LAST_USERNAME_KEY = "LAST_USERNAME";
-
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(UserController.class);
     @Autowired
     private AclEntryDao aclEntryDao;
     @Autowired
@@ -83,11 +83,14 @@ public class UserController {
     private ApplicationMailer applicationMailer;
     @Autowired
     private ConfigurationDao configurationDao;
-
     @Autowired
     private CacheService cacheService;
     @Autowired
     private MoPatActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider;
+    @Autowired
+    private PinAuthorizationService pinAuthorizationService;
+    @Autowired
+    private PinAuthorizationDao pinAuthorizationDao;
 
     /**
      * @param id (<i>optional</i>) Id of the {@link User User} object
@@ -202,6 +205,7 @@ public class UserController {
                     user.setPassword(newPassword);
                     userDao.setPassword(user);
                 }
+                userDao.setPin(user);
                 userDao.merge(user);
             }
         }
@@ -443,10 +447,16 @@ public class UserController {
             user.setNewPassword(newPassword);
             userValidator.validate(user, result);
             if (!result.hasErrors()) {
+
                 if (!isLdap) {
                     userDao.setPassword(user);
                 }
+
+                if (user.getUsePin()) {
+                    userDao.setPin(user);
+                }
                 userDao.merge(user);
+
                 // get newly persisted user
                 User persistedUser = userDao.loadUserByUsername(user.getUsername());
                 // grant clinic rights
@@ -513,8 +523,13 @@ public class UserController {
     @RequestMapping(value = "/mobile/user/login", method = RequestMethod.GET)
     public String login(@RequestParam(value = "message", required = false) final String message,
         final Model model, final HttpSession httpSession) {
-        // Logout first!
-        SecurityContextHolder.getContext().setAuthentication(null);
+        // Logout or redirect to Pin Auth first!
+        if (pinAuthorizationService.isPinLoginApplicable()) {
+            return "mobile/user/pinlogin";
+        } else {
+            SecurityContextHolder.getContext().setAuthentication(null);
+        }
+
         if (message != null && !message.isEmpty()) {
             switch (message) {
                 case "InsufficientAuthenticationException":
@@ -545,6 +560,91 @@ public class UserController {
         model.addAttribute("defaultLanguage", defaultLanguage);
         model.addAttribute("encounter", null);
         return "mobile/user/login";
+    }
+
+    /**
+     * Control the HTTP GET requests for the URL <i>/mobile/user/pinlogin</i> Shows the pin login
+     * page
+     *
+     * @return The <i>/mobile/user/login</i> view
+     */
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = "mobile/user/pinlogin", method = RequestMethod.GET)
+    public String pinLogin(final Model model) {
+        return "mobile/user/pinlogin";
+    }
+
+    /**
+     * Control the HTTP POST requests for the URL <i>/mobile/user/pinlogin</i> Checks if the entered
+     * PIN for a logged in user matches with the stored hash in the database. If it matches, the
+     * flags for the pin login are removed from the database Performs a manaual role based redirect
+     * afterwards
+     *
+     * @return <i>/admin/index</i> for ROLE_ADMIN, <i>/mobile/survey/index</i> else
+     */
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = "mobile/user/pinlogin", method = RequestMethod.POST)
+    public String pinLogin(@RequestParam("pin") String pin, HttpServletRequest httpServletRequest,
+        final Model model) {
+        //Get the current user
+        User user;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()
+            && !(authentication.getPrincipal() instanceof String)) {
+            user = userDao.loadUserByUsername(authentication.getName());
+        } else {
+            //If this is ever the case, something went wrong with Spring. Then clear the session and redirect to normal login
+            SecurityContextHolder.getContext().setAuthentication(null);
+            return "mobile/user/login";
+        }
+
+        PinAuthorization pinAuthorization = pinAuthorizationDao.getEntriesForUser(user).stream()
+            .toList().get(0);
+
+        if (userDao.isCorrectPin(user, pin)) {
+            //If pin is entered correctly, the filter has to be stopped by removing all entries from db
+            pinAuthorizationService.removePinAuthForUser(user);
+        } else {
+            pinAuthorizationService.decreaseRemainingTriesForUser(user);
+            model.addAttribute("message",
+                messageSource.getMessage("user.error.badPin", new Object[]{
+                    String.valueOf(pinAuthorization.getRemainingTries())
+                }, LocaleContextHolder.getLocale()));
+            return "mobile/user/pinlogin";
+        }
+
+        //Handle Role based redirect manually here
+        if (httpServletRequest.isUserInRole("ROLE_ADMIN")) {
+            return "redirect:/admin/index";
+        } else {
+            return "redirect:/mobile/survey/index";
+        }
+    }
+
+    /**
+     * Control the HTTP GET requests for the URL <i>/mobile/user/pinlogout</i> Clears the current
+     * session, removes the pin login flag for a user and redirects to the login page
+     *
+     * @return The <i>/mobile/user/login</i> view
+     */
+    @RequestMapping(value = "/mobile/user/pinlogout", method = RequestMethod.GET)
+    public String deactivatePinLoginAndLogout() {
+        //Get the current user
+        User user;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()
+            && !(authentication.getPrincipal() instanceof String)) {
+            user = userDao.loadUserByUsername(authentication.getName());
+        } else {
+            //If this is ever the case, something went wrong with Spring. Then clear the session and redirect to normal login
+            SecurityContextHolder.getContext().setAuthentication(null);
+            return "mobile/user/login";
+        }
+
+        pinAuthorizationService.removePinAuthForUser(user);
+        SecurityContextHolder.getContext().setAuthentication(null);
+        return "mobile/user/login";
+
     }
 
     /**
