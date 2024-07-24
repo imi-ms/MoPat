2c0bc89fb1e4356c3018c0b844c33ddc00fe653c
schluebbo <lueder.jungjohann@t-online.de>
2024-07-24 14:07:55 +0200

    refactor: Introduced QuestionnaireFactory to simplify questionnaire creation and improve testability

---
+++ src/main/java/de/imi/mopat/helper/model/QuestionnaireFactory.java
@@ -0,0 +1,17 @@
+package de.imi.mopat.helper.model;
+
+import de.imi.mopat.model.Questionnaire;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestionnaireFactory {
+    public Questionnaire createQuestionnaire(String name, String description, Long changedBy, Long createdBy, Boolean isPublished) {
+        return new Questionnaire(
+                name,
+                description,
+                changedBy,
+                createdBy,
+                isPublished
+        );
+    }
+}
