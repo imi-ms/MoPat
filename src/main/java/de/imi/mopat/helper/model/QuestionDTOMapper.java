1b24a2c2438780bf1d3bf7eb6bd8ef4e352d5598
schluebbo <lueder.jungjohann@t-online.de>
2024-07-24 13:01:06 +0200

    feat: Extracted QuestionDTOMapper from QuestionService for improved modularity, utilizing AnswerDTOMapper

---
+++ src/main/java/de/imi/mopat/helper/model/QuestionDTOMapper.java
@@ -0,0 +1,68 @@
+package de.imi.mopat.helper.model;
+
+import de.imi.mopat.dao.ConfigurationDao;
+import de.imi.mopat.helper.controller.Constants;
+import de.imi.mopat.model.*;
+import de.imi.mopat.model.dto.AnswerDTO;
+import de.imi.mopat.model.dto.QuestionDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.function.Function;
+
+@Component
+public class QuestionDTOMapper implements Function<Question, QuestionDTO> {
+
+    private static final int FRONT = 0;
+    private static final int BACK = 1;
+    private static final int FRONT_BACK = 2;
+    private final AnswerDTOMapper answerDTOMapper;
+
+    public QuestionDTOMapper(ConfigurationDao configurationDao) {
+        this.answerDTOMapper = new AnswerDTOMapper(configurationDao);
+    }
+
+    @Override
+    public QuestionDTO apply(Question question) {
+        List<String> images = new ArrayList<>();
+        SortedMap<Long, AnswerDTO> answerDTOs = new TreeMap<>();
+
+        question.getAnswers().forEach(answer -> {
+            AnswerDTO answerDTO = answerDTOMapper.apply(answer);
+            answerDTOs.put((long) answerDTOs.size(), answerDTO);
+
+            if (answer instanceof BodyPartAnswer bodyPartAnswer
+                    && !images.contains(bodyPartAnswer.getBodyPart().getImagePath())
+            ) {
+                images.add(bodyPartAnswer.getBodyPart().getImagePath());
+            }
+        });
+
+        QuestionDTO questionDTO = new QuestionDTO();
+        questionDTO.setId(question.getId());
+        questionDTO.setQuestionType(question.getQuestionType());
+        questionDTO.setLocalizedQuestionText(new TreeMap<>(question.getLocalizedQuestionText()));
+        questionDTO.setIsRequired(question.getIsRequired());
+        questionDTO.setIsEnabled(question.getIsEnabled());
+        questionDTO.setMinNumberAnswers(question.getMinNumberAnswers());
+        questionDTO.setMaxNumberAnswers(question.getMaxNumberAnswers());
+        questionDTO.setCodedValueType(question.getCodedValueType());
+        questionDTO.setPosition(question.getPosition());
+        questionDTO.setQuestionnaireId(question.getQuestionnaire().getId());
+        questionDTO.setAnswers(answerDTOs);
+        questionDTO.setBodyPartImages(images);
+        questionDTO.setImageType(determineImageType(images));
+        return questionDTO;
+    }
+
+    private String determineImageType(List<String> images) {
+        if (images.contains(Constants.BODY_FRONT) && images.contains(Constants.BODY_BACK)) {
+            return Constants.BODY_PART_IMAGE_TYPES[FRONT_BACK];
+        } else if (images.contains(Constants.BODY_FRONT)) {
+            return Constants.BODY_PART_IMAGE_TYPES[FRONT];
+        } else if (images.contains(Constants.BODY_BACK)) {
+            return Constants.BODY_PART_IMAGE_TYPES[BACK];
+        }
+        return null;
+    }
+}
