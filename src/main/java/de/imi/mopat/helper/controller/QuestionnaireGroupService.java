06929c3b8903202e4f27bb572a9661ff1b4b71c6
Lüder Jungjohann <lueder.jungjohann@uni-muenster.de>
2024-08-01 12:26:51 +0200

    Update QuestionnaireGroupService to handle group operations without getNextGroupId

--- src/main/java/de/imi/mopat/helper/controller/QuestionnaireGroupService.java
+++ src/main/java/de/imi/mopat/helper/controller/QuestionnaireGroupService.java
@@ -1,21 +1,31 @@
 package de.imi.mopat.helper.controller;
 
 import de.imi.mopat.dao.QuestionnaireGroupDao;
+import de.imi.mopat.helper.model.QuestionnaireGroupDTOMapper;
 import de.imi.mopat.model.Questionnaire;
 import de.imi.mopat.model.QuestionnaireGroup;
+import de.imi.mopat.model.QuestionnaireGroupMember;
+import de.imi.mopat.model.dto.QuestionnaireGroupDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class QuestionnaireGroupService {
 
-    private QuestionnaireGroupDao questionnaireGroupDao;
+    private final QuestionnaireGroupDao questionnaireGroupDao;
+
+    private final QuestionnaireGroupDTOMapper questionnaireGroupDTOMapper;
 
     @Autowired
-    public void setQuestionnaireGroupDao(QuestionnaireGroupDao questionnaireGroupDao) {
+    public QuestionnaireGroupService(QuestionnaireGroupDao questionnaireGroupDao, QuestionnaireGroupDTOMapper questionnaireGroupDTOMapper) {
         this.questionnaireGroupDao = questionnaireGroupDao;
+        this.questionnaireGroupDTOMapper = questionnaireGroupDTOMapper;
     }
 
     /**
@@ -26,14 +36,99 @@ public class QuestionnaireGroupService {
      * @param existingQuestionnaire the existing questionnaire to base the grouping on
      */
     public void saveGroupInformation(Questionnaire newQuestionnaire, Questionnaire existingQuestionnaire) {
-        if (newQuestionnaire == null || existingQuestionnaire == null) {
-            throw new IllegalArgumentException("Questionnaires must not be null");
-        }
+        validateQuestionnaires(newQuestionnaire, existingQuestionnaire);
         if (existingQuestionnaire.isOriginal()) {
             handleOriginalQuestionnaire(newQuestionnaire, existingQuestionnaire);
         } else {
             handleDuplicateQuestionnaire(newQuestionnaire, existingQuestionnaire);
         }
+    }
+
+    /**
+     * Checks if a questionnaire is already part of a group.
+     *
+     * @param questionnaire the questionnaire to check
+     * @return true if the questionnaire is in a group, false otherwise
+     */
+    public boolean isQuestionnaireInGroup(Questionnaire questionnaire) {
+        validateQuestionnaires(questionnaire);
+        List<QuestionnaireGroup> allElements = questionnaireGroupDao.getAllElements();
+        return allElements.stream()
+                .flatMap(group -> group.getQuestionnaireGroupMembers().stream())
+                .anyMatch(member -> member.getQuestionnaire().equals(questionnaire));
+    }
+
+    /**
+     * Retrieves the group for a specified questionnaire if it exists.
+     *
+     * @param questionnaire the questionnaire to check
+     * @return an Optional containing the group if it exists, or an empty Optional
+     */
+    public Optional<QuestionnaireGroup> findGroupForQuestionnaire(Questionnaire questionnaire) {
+        return questionnaireGroupDao.getAllElements().stream()
+                .filter(group -> group.getQuestionnaireGroupMembers().stream()
+                        .anyMatch(member -> member.getQuestionnaire().equals(questionnaire)))
+                .findFirst();
+    }
+
+    /**
+     * Finds the maximum version number among the questionnaires in the specified group.
+     *
+     * @param questionnaireGroup the group for which to find the maximum version number
+     * @return the maximum version number in the group. If the group is empty, returns 1.
+     */
+    public int findMaxVersionInGroup(QuestionnaireGroup questionnaireGroup) {
+        return questionnaireGroup.getQuestionnaireGroupMembers().stream()
+                .map(QuestionnaireGroupMember::getQuestionnaire)
+                .map(Questionnaire::getVersion)
+                .max(Integer::compareTo)
+                .orElse(1);
+    }
+
+    /**
+     * Retrieves the group by its ID.
+     *
+     * @param groupId the ID of the group to retrieve
+     * @return an Optional containing the group if it exists, or an empty Optional
+     */
+    public Optional<QuestionnaireGroup> getGroupById(Long groupId){
+        return questionnaireGroupDao.getAllElements().stream()
+                .filter(group -> group.getId().equals(groupId))
+                .findFirst();
+    }
+
+    /**
+     * Retrieves all QuestionnaireGroupMembers.
+     *
+     * @return a list of all QuestionnaireGroupMembers
+     */
+    private List<QuestionnaireGroupMember> getAllQuestionnaireGroupMembers() {
+        return questionnaireGroupDao.getAllElements().stream()
+                .flatMap(group -> group.getQuestionnaireGroupMembers().stream())
+                .toList();
+    }
+
+    /**
+     * Retrieves all unique group IDs.
+     *
+     * @return a set of all unique group IDs
+     */
+    public Set<Long> getAllUniqueGroupIds() {
+        return questionnaireGroupDao.getAllElements().stream()
+                .map(QuestionnaireGroup::getId)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Retrieves all QuestionnaireGroupDTOs.
+     *
+     * @return a list of all QuestionnaireGroupDTOs
+     */
+    public List<QuestionnaireGroupDTO> getAllQuestionnaireGroupDTOs() {
+        return questionnaireGroupDao.getAllElements().stream()
+                .map(questionnaireGroupDTOMapper)
+                .sorted(Comparator.comparing(QuestionnaireGroupDTO::getGroupName))
+                .toList();
     }
 
     /**
@@ -43,14 +138,14 @@ public class QuestionnaireGroupService {
      * @param originalQuestionnaire the original questionnaire
      */
     private void handleOriginalQuestionnaire(Questionnaire newQuestionnaire, Questionnaire originalQuestionnaire) {
-        Optional<Long> groupIdForQuestionnaire = getGroupIdForQuestionnaire(originalQuestionnaire);
-        if (groupIdForQuestionnaire.isEmpty()) {
-            Long newGroupId = questionnaireGroupDao.getNextGroupId();
-            saveQuestionnaireGroup(newGroupId, originalQuestionnaire);
-            saveQuestionnaireGroup(newGroupId, newQuestionnaire);
+        Optional<QuestionnaireGroup> groupForQuestionnaire = findGroupForQuestionnaire(originalQuestionnaire);
+        if (groupForQuestionnaire.isPresent()) {
+            saveQuestionnaireToGroup(groupForQuestionnaire.get(), newQuestionnaire);
         }else{
-            Long existingGroupId = groupIdForQuestionnaire.get();
-            saveQuestionnaireGroup(existingGroupId, newQuestionnaire);
+            QuestionnaireGroup newQuestionnaireGroup = new QuestionnaireGroup();
+            newQuestionnaireGroup.setName(originalQuestionnaire.getName());
+            saveQuestionnaireToGroup(newQuestionnaireGroup, originalQuestionnaire);
+            saveQuestionnaireToGroup(newQuestionnaireGroup, newQuestionnaire);
         }
     }
 
@@ -61,66 +156,55 @@ public class QuestionnaireGroupService {
      * @param existingQuestionnaire the existing duplicate questionnaire
      */
     private void handleDuplicateQuestionnaire(Questionnaire newQuestionnaire, Questionnaire existingQuestionnaire) {
-        Optional<Long> groupIdOpt = getGroupIdForQuestionnaire(existingQuestionnaire);
-        Long groupId = groupIdOpt.orElseGet(questionnaireGroupDao::getNextGroupId);
-        if (groupIdOpt.isEmpty()) {
-            saveQuestionnaireGroup(groupId, existingQuestionnaire);
+        QuestionnaireGroup group = findGroupForQuestionnaire(existingQuestionnaire)
+                .orElseGet(() -> {
+                    QuestionnaireGroup newGroup = new QuestionnaireGroup();
+                    newGroup.setName(existingQuestionnaire.getName());
+                    return newGroup;
+                });
+        if (group.getQuestionnaireGroupMembers().isEmpty()) {
+            saveQuestionnaireToGroup(group, existingQuestionnaire);
         }
-        saveQuestionnaireGroup(groupId, newQuestionnaire);
+        saveQuestionnaireToGroup(group, newQuestionnaire);
+    }
+
+
+    /**
+     * Validates that the provided questionnaires are not null.
+     *
+     * @param questionnaires the questionnaires to validate
+     */
+    private void validateQuestionnaires(Questionnaire... questionnaires) {
+        for (Questionnaire questionnaire : questionnaires) {
+            if (questionnaire == null) {
+                throw new IllegalArgumentException("Questionnaires must not be null");
+            }
+        }
     }
 
     /**
-     * Checks if a questionnaire is already part of a group.
+     * Saves a questionnaire to a specified group.
      *
-     * @param questionnaire the questionnaire to check
-     * @return true if the questionnaire is in a group, false otherwise
+     * @param questionnaireGroup the group to save the questionnaire to
+     * @param questionnaire the questionnaire to be saved
      */
-    public boolean isQuestionnaireInGroup(Questionnaire questionnaire) {
-        if (questionnaire == null) {
-            throw new IllegalArgumentException("Questionnaires must not be null");
-        }
-        return questionnaireGroupDao.getAllElements().stream()
-                .anyMatch(group -> group.getQuestionnaire().equals(questionnaire));
-    }
-
-    /**
-     * Saves the group information for a questionnaire.
-     *
-     * @param groupId the group ID to assign
-     * @param questionnaire the questionnaire to be grouped
-     */
-    private void saveQuestionnaireGroup(Long groupId, Questionnaire questionnaire) {
-        QuestionnaireGroup questionnaireGroup = new QuestionnaireGroup();
-        questionnaireGroup.setGroupId(groupId);
-        questionnaireGroup.setQuestionnaire(questionnaire);
+    private void saveQuestionnaireToGroup(QuestionnaireGroup questionnaireGroup, Questionnaire questionnaire) {
+        QuestionnaireGroupMember newMember = createQuestionnaireGroupMember(questionnaireGroup, questionnaire);
+        questionnaireGroup.addMember(newMember);
         questionnaireGroupDao.merge(questionnaireGroup);
     }
 
     /**
-     * Retrieves the group ID for a questionnaire if it exists.
+     * Creates a new QuestionnaireGroupMember.
      *
-     * @param questionnaire the questionnaire to check
-     * @return an Optional containing the group ID if it exists, or an empty Optional
+     * @param questionnaireGroup the group the member belongs to
+     * @param questionnaire the questionnaire of the member
+     * @return the created QuestionnaireGroupMember
      */
-    public Optional<Long> getGroupIdForQuestionnaire(Questionnaire questionnaire) {
-        return questionnaireGroupDao.getAllElements().stream()
-                .filter(group -> group.getQuestionnaire().equals(questionnaire))
-                .map(QuestionnaireGroup::getGroupId)
-                .findFirst();
-    }
-
-    /**
-     * Finds the maximum version number among the questionnaires in the specified group.
-     *
-     * @param groupId The ID of the group for which to find the maximum version number.
-     * @return The maximum version number in the group. If the group is empty, returns 0.
-     */
-    public int findMaxVersionInGroup(Long groupId) {
-        return questionnaireGroupDao.getAllElements().stream()
-                .filter(group -> group.getGroupId().equals(groupId))
-                .map(group -> group.getQuestionnaire().getVersion())
-                .max(Integer::compare)
-                .orElse(0);
+    private QuestionnaireGroupMember createQuestionnaireGroupMember(QuestionnaireGroup questionnaireGroup, Questionnaire questionnaire) {
+        QuestionnaireGroupMember newMember = new QuestionnaireGroupMember();
+        newMember.setQuestionnaireGroup(questionnaireGroup);
+        newMember.setQuestionnaire(questionnaire);
+        return newMember;
     }
 }
-
