32fd312ffbd7b3777d2bb56d92597d4375e630e5
schluebbo <lueder.jungjohann@t-online.de>
2024-08-16 11:42:44 +0200

    Fix: Add @JsonIgnore to questionnaireGroupDTO to prevent circular reference issues during JSON serialization in surveys

--- src/main/java/de/imi/mopat/model/dto/QuestionnaireDTO.java
+++ src/main/java/de/imi/mopat/model/dto/QuestionnaireDTO.java
@@ -56,6 +56,7 @@ public class QuestionnaireDTO {
 
     private List<QuestionDTO> questionDTOs;
 
+    @JsonIgnore
     private QuestionnaireGroupDTO questionnaireGroupDTO;
 
     public List<QuestionDTO> getQuestionDTOs() {
