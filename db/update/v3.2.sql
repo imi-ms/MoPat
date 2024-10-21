87c774475d440355f3272a761397dd4c3474bb96
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-10-21 10:44:14 +0200

    Added update script to add new retriever to configuration options

---
+++ db/update/v3.2.sql
@@ -0,0 +1,5 @@
+USE moPat;
+
+INSERT INTO moPat.SelectConfiguration_OPTIONS
+(SelectConfiguration_id, `options`)
+VALUES(33, 'de.imi.mopat.helper.controller.HL7v22PatientInformationRetrieverByPID');
