185528899d08cab03df45069dac0302b0368a148
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-10-24 14:34:16 +0200

    Fix update script using the wrong db names

--- db/update/v3.2.sql
+++ db/update/v3.2.sql
@@ -1,4 +1,4 @@
-USE `mopat_user`;
+USE `moPat_user`;
 
 CREATE TABLE IF NOT EXISTS `pin_authorization` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -15,7 +15,7 @@ ALTER TABLE `pin_authorization`
 ALTER TABLE moPat_user.acl_sid ADD use_pin TINYINT(1) DEFAULT FALSE NULL;
 ALTER TABLE moPat_user.acl_sid ADD pin varchar(255) DEFAULT NULL NULL;#
 
-USE `mopat`;
+USE `moPat`;
 
 INSERT INTO `configuration` (`id`, `type`, `configuration_group_id`, `parent`, `position`, `attribute`, `configuration_type`, `description_message_code`, `class`, `label_message_code`, `test_method`, `update_method`, `uuid`, `value`, `pattern`) VALUES
 (78, 'GENERAL', 1, NULL, 15, 'enableGlobalPinAuth', 'BOOLEAN', NULL, 'GLOBAL', 'configuration.label.enableGlobalPinAuth', NULL, NULL, '5c4ca0df-fe5e-4582-8e9e-e290e1ed8efe', 'true', NULL);
