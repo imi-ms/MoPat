9bac58aa0e79687f3ac14d9aeebbb60e1986f48c
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-12-10 15:40:14 +0100

    Added basic selenium test in python

---
+++ selenium/selenium_tests.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from time import gmtime, strftime
+import unittest
+import json
+import os
+from abc import ABC, abstractmethod
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from helper.Login import LoginHelper
+
+loginHelper = LoginHelper()
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# noinspection PyStatementEffect
+class IMISeleniumBaseTest(ABC):
+    """
+        Base class of all IMI selenium tests.
+        * Handles the initialization of the driver.
+        * Switches between server and local mode
+    """
+
+    currentResult = None
+    """ Attribute used to set a cookie for validation purposes. """
+    driver = None
+    """ Selenium driver used in the tests. """
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+            Used to initialize constants.
+        """
+        # get filename of calling script (url)
+        url = "127.0.0.1:8080/"
+        cls.base_url = url
+        cls.https_base_url = f"http://{url}"
+        # secret used in the subclass
+        cls.secret = cls._loadSecretFile(cls, "secret")
+        cls.selenium_grid_url = None
+
+    def setUp(self) -> None:
+        """
+            Start a new driver for each test.
+            Checks, if the script is called on the server or locally.
+        """
+        
+        try:
+            # create folder for driver
+            directory = os.path.join(os.getcwd(), "driver")
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            # init driver
+            self._setLocalDriver(directory)
+        except Exception as e:
+            print(e)
+
+
+        # maximize window to full-screen
+        self.driver.maximize_window()
+
+    def run(self, result=None) -> None:
+        """
+            Main function of the unit test.
+        """
+        self.currentResult = result
+        unittest.TestCase.run(self, result)
+
+    def tearDown(self) -> None:
+        """
+            Sets the cookie to validate, if the test was successful or not.
+        """
+        if self.currentResult.wasSuccessful():
+            cookie = {'name': 'zaleniumTestPassed', 'value': 'true'}
+        else:
+            cookie = {'name': 'zaleniumTestPassed', 'value': 'false'}
+        self.driver.add_cookie(cookie)
+        self.driver.quit()
+
+    def _loadSecretFile(self, filename):
+        """
+        Used to try loading file from server or locally.
+
+        :param filename: file to be loaded without .json
+        :return: The loaded json object or None
+        """
+        secret_local = os.path.join(os.getcwd(), "secrets", f"{filename}.json")
+        if os.path.exists(secret_local):
+            with open(secret_local) as f:
+                return json.load(f)
+        return None
+
+    @abstractmethod
+    def _setServerDriver(self):
+        self.driver = None
+
+    @abstractmethod
+    def _setLocalDriver(self, directory):
+        self.driver = None
+
+
+
+class CustomTest(IMISeleniumBaseTest):
+    def test_login_admin(self):
+        if(self.secret['admin-username']!='' and self.secret['admin-password']!=''):
+            self.driver.get(self.https_base_url)
+
+            loginHelper.login(self.driver,self.secret['admin-username'], self.secret['admin-password'])
+
+            WebDriverWait(self.driver, 10).until(EC.url_to_be(self.https_base_url + "admin/index"))
+
+            self.driver.find_element(By.CSS_SELECTOR, "#headerNav > div:nth-child(2) > li:nth-child(3) > a:nth-child(1)").click()
+
+            WebDriverWait(self.driver, 10).until(EC.url_to_be(self.https_base_url + "mobile/user/login?lang=de_DE"))
+
+            assert True
+        else:
+            pass
+    
+    def tearDown(self): 
+        self.driver.quit()
+
+
+class IMISeleniumChromeTest(IMISeleniumBaseTest):
+    """
+        Test class for Chrome tests.
+    """
+    def _setServerDriver(self):
+        name: str = f"{strftime('%Y-%m-%d-%H-%M-%S', gmtime())}_{self.base_url}_chrome"
+        options = webdriver.ChromeOptions()
+        options.set_capability("acceptInsecureCerts", True)
+        options.set_capability("selenoid:options", {
+                                                    "enableVNC": True,
+                                                    "enableVideo": True,
+                                                    "enableLog": True,
+                                                    "name": name,
+                                                    "videoName": f"{name}.mp4",
+                                                    "logName": f"{name}.log"
+                                                    })
+        self.driver = webdriver.Remote(options=options,
+                                       command_executor=self.selenium_grid_url)
+
+
+    def _setLocalDriver(self, directory):
+        # download latest driver
+        from selenium.webdriver.chrome.service import Service
+        from webdriver_manager.chrome import ChromeDriverManager
+        from webdriver_manager.core.driver_cache import DriverCacheManager
+        # init driver
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(cache_manager=DriverCacheManager(directory)).install()))
+
+
+class CustomChromeTest(CustomTest, IMISeleniumChromeTest, unittest.TestCase):
+    # Do not touch this function. This is the main entry point for selenium
+    pass
+
+        
+if __name__ == "__main__":
+    unittest.main()
