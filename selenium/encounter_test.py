ec45406b2240f0586c90f65b0ccbb7fe5b5639f6
Hussain Sabir <hussainaliasghar053@gmail.com>
2025-01-17 15:34:27 +0100

    manage survey page tests

---
+++ selenium/encounter_test.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import random
+import string
+import time
+import unittest
+import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from helper.Authentication import AuthenticationHelper
+from helper.Bundle import BundleHelper, BundleSelectors
+from helper.Clinic import ClinicHelper, ClinicSelectors
+from helper.Navigation import NavigationHelper
+from helper.Question import QuestionHelper, QuestionErrorHelper, QuestionSelectors
+from helper.Questionnaire import QuestionnaireHelper
+from helper.Condition import ConditionHelper, ConditionSelectors
+from helper.Score import ScoreHelper
+from helper.SeleniumUtils import SeleniumUtils, ErrorSelectors
+from helper.Survey import SurveyHelper, SurveySelectors, SurveyAssertHelper
+from helper.Language import LanguageHelper, LanguageSelectors
+from helper.User import UserSelector
+from helper.Encounter import EncounterSelectors, EncounterHelper
+from utils.imiseleniumtest import IMISeleniumBaseTest, IMISeleniumChromeTest
+from selenium.common.exceptions import TimeoutException
+
+
+class URLPaths:
+    ADMIN_INDEX = "/admin/index"
+    ADMIN_INDEX_DE = "/admin/index?lang=de_DE"
+    MOBILE_USER_LOGIN = "/mobile/user/login"
+    MOBILE_USER_LOGIN_DE = "/mobile/user/login?lang=de_DE"
+    MOBILE_SURVEY_INDEX = "/mobile/survey/index"
+    LOGIN_BAD_CREDENTIALS = "/mobile/user/login?message=BadCredentialsException"
+    LOGIN_DISABLED_EXCEPTION = "/mobile/user/login?message=DisabledException"
+    PASSWORD_FORGOT = "/mobile/user/password"
+
+
+class EmailSelectors:
+    SUBJECT_INPUT = (By.ID, "subject")
+    CONTENT_INPUT = (By.ID, "mailContent")
+    MAIL_PREVIEW_BUTTON = (By.ID, "mailPreviewButton")
+    SEND_BUTTON = (By.ID, "mailButton")
+
+
+class PasswordResetSelectors:
+    FORGOT_PASSWORD_LINK = (By.ID, "forgotPasswordLink")
+    USERNAME_INPUT = (By.ID, "username")
+    ERROR_MESSAGE = (By.CLASS_NAME, "error")
+
+
+class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
+    seleniumMode: IMISeleniumBaseTest.SeleniumMode = IMISeleniumBaseTest.SeleniumMode.LOCAL
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.base_url = "localhost:8080"
+        cls.https_base_url = "http://localhost:8080"
+
+    def setUp(self):
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("start-maximized");
+        driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
+        # Initialize the WebDriver
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+        # Initialize Navigation and Utils
+        self.navigation_helper = NavigationHelper(self.driver)
+        self.utils = SeleniumUtils(self.driver, navigation_helper=self.navigation_helper)
+        self.navigation_helper.utils = self.utils
+
+        # Initialize other helpers
+        self.authentication_helper = AuthenticationHelper(self.driver)
+        self.questionnaire_helper = QuestionnaireHelper(self.driver, navigation_helper=self.navigation_helper)
+        self.question_helper = QuestionHelper(self.driver, navigation_helper=self.navigation_helper)
+        self.condition_helper = ConditionHelper(self.driver, navigation_helper=self.navigation_helper)
+        self.score_helper = ScoreHelper(self.driver, navigation_helper=self.navigation_helper)
+        self.questionnaire_error_helper = QuestionErrorHelper(self.driver, navigation_helper=self.navigation_helper)
+
+        self.bundle_helper = BundleHelper(self.driver, self.navigation_helper)
+        self.clinic_helper = ClinicHelper(self.driver, self.navigation_helper)
+        self.survey_helper = SurveyHelper(self.driver, self.navigation_helper)
+        self.encounter_helper = EncounterHelper(self.driver, self.navigation_helper)
+
+        self.survey_assert_helper = SurveyAssertHelper(self.driver, self.authentication_helper)
+        self.language_helper = LanguageHelper(self.driver)
+
+    def test_encounter_list(self):
+        # Arrange
+        if not self.secret.get('admin-username') or not self.secret.get('admin-username'):
+            self.skipTest("User AD credentials missing. Test skipped.")
+        self.driver.get(self.https_base_url)
+        self.authentication_helper.login(self.secret['admin-username'], self.secret['admin-password'])
+
+        # Act
+        self.navigation_helper.navigate_to_manage_surveys()
+
+        #Assert - Check if tabs for "All Encounters" and "Scheduled Encounters" are present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(EncounterSelectors.BUTTON_ENCOUNTER_TABLE)
+            )
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(EncounterSelectors.BUTTON_ENCOUNTER_SCHEDULE_TABLE)
+            )
+        except TimeoutException:
+            self.fail("Encounter tabs not found")
+        
+        # Act - Click on "All Encounters" tab
+        self.utils.click_element(EncounterSelectors.BUTTON_ENCOUNTER_TABLE)
+        # Assert - Check if the table for all encounters is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.TABLE_ALL_ENCOUNTERS)
+            )
+        except TimeoutException:
+            self.fail("All Encounters table not found")
+        
+        #Assert - Check if table pagination is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.PAGINATION_ENCOUNTER_TABLE)
+            )
+        except TimeoutException:
+            self.fail("Pagination for All Encounters table not found")
+
+        #Assert - Check if table search is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.SEARCH_ALL_ENCOUNTERS)
+            )
+        except TimeoutException:
+            self.fail("Search for All Encounters table not found")
+
+        #TODO: Action column, number of exports [after create survey function implementation]
+
+        #Assert - Check for button for starting an encounter
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(EncounterSelectors.BUTTON_EXECUTE_ENCOUNTER)
+            )
+        except TimeoutException:
+            self.fail("Execute Encounter button not found")
+
+
+        # Act - Click on "Scheduled Encounters" tab
+        self.utils.click_element(EncounterSelectors.BUTTON_ENCOUNTER_SCHEDULE_TABLE)
+        # Assert - Check if the table for scheduled encounters is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.TABLE_SCHEDULED_ENCOUNTERS)
+            )
+        except TimeoutException:
+            self.fail("Scheduled Encounters table not found")
+
+        #Assert - Check if table for scheduled encounters is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.TABLE_SCHEDULED_ENCOUNTERS)
+            )
+        except TimeoutException:
+            self.fail("Scheduled Encounters table not found")
+
+        #Assert - Check if table pagination is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.PAGINATION_ENCOUNTER_SCHEDULE_TABLE)
+            )
+        except TimeoutException:
+            self.fail("Pagination for Scheduled Encounters table not found")
+        
+        #Assert - Check if table search is present
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(EncounterSelectors.SEARCH_SCHEDULED_ENCOUNTERS)
+            )
+        except TimeoutException:
+            self.fail("Search for Scheduled Encounters table not found")
+
+        #TODO: Action column, number of exports [after survey schedule function implementation]
+
+        #Assert - Check for button for scheduling an encounter
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(EncounterSelectors.BUTTON_SCHEDULE_ENCOUNTER)
+            )
+        except TimeoutException:
+            self.fail("Schedule Encounter button not found")
+        
+    def tearDown(self):
+        if self.driver:
+            self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
--- selenium/helper/Encounter.py
+++ selenium/helper/Encounter.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import time
 
 from selenium.common import TimeoutException
@@ -11,6 +12,13 @@ from helper.Navigation import NavigationHelper
 from helper.Question import QuestionSelectors
 from helper.SeleniumUtils import SeleniumUtils
 
+class EncounterScheduleType(Enum):
+    UNIQUELY = "UNIQUELY"
+    REPEATEDLY = "REPEATEDLY"
+    WEEKLY = "WEEKLY"
+    MONTHLY = "MONTHLY"
+
+
 class EncounterSelectors:
     BUTTON_ENCOUNTER_TABLE = (By.ID, "encounter-tablink")
     BUTTON_ENCOUNTER_SCHEDULE_TABLE = (By.ID, "encounterScheduled-tablink")
@@ -20,11 +28,20 @@ class EncounterSelectors:
     TABLE_ALL_ENCOUNTERS = (By.ID, "encounterTable")
     TABLE_SCHEDULED_ENCOUNTERS = (By.ID, "encounterScheduled")
 
+    PAGINATION_ENCOUNTER_SCHEDULE_TABLE = (By.ID, "encounterScheduled_paginate")
+    PAGINATION_ENCOUNTER_TABLE = (By.ID, "encounterTable_paginate")
+
+    SEARCH_ALL_ENCOUNTERS = (By.ID, "encounterTable_filter")
+    SEARCH_SCHEDULED_ENCOUNTERS = (By.ID, "encounterScheduled_filter")
+
 class EncounterHelper:
 
     def __init__(self, driver: WebDriver, navigation_helper: NavigationHelper):
         self.driver = driver
         self.utils = SeleniumUtils(driver)
         self.navigation_helper = navigation_helper
+
+    def schedule_encounter(case_number, clinic, bundle, email, start_date, time_period_days, end_date, language, message):
+        pass
 
     
