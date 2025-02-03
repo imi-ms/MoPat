133acf77589239f4023417464b857f8211264fc4
Hussain Sabir <hussainaliasghar053@gmail.com>
2025-02-03 11:01:45 +0100

    ThymeleafLocalHelper class for converting list dates to locale format

---
+++ src/main/java/de/imi/mopat/helper/controller/ThymeleafLocalHelper.java
@@ -0,0 +1,19 @@
+package de.imi.mopat.helper.controller;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ThymeleafLocalHelper {
+
+    public static String formatTimestampToLocaleDate(Timestamp timestamp) {
+        Locale locale = Locale.getDefault();
+        Date date = new Date(timestamp.getTime());
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG, locale);
+        return dateFormat.format(date);
+    }
+}
