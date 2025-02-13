9ab28ee43218786507491786761a3227074e36da
Hussain Sabir <hussainaliasghar053@gmail.com>
2025-02-13 14:24:31 +0100

    update bundle_tests

--- selenium/bundle_test.py
+++ selenium/bundle_test.py
@@ -86,8 +86,9 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
         self.language_helper = LanguageHelper(self.driver, self.navigation_helper)
 
     def test_bundle_list(self):
-        bundle_name = "Test Bundle"
-        clinic_name = "Test Clinic"
+        bundle={}
+        created_questionnaire={}
+        clinic={}
 
         # Arrange
         if not self.secret.get('admin-username') or not self.secret.get('admin-username'):
@@ -96,10 +97,7 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
         self.authentication_helper.login(self.secret['admin-username'], self.secret['admin-password'])
 
         try:
-            created_questionnaire = self.questionnaire_helper.create_questionnaire_with_questions(questionnaire_name="Test", questionnaire_description="Test",
-                                            questionnaire_language_code="de_DE", questionnaire_display_name="Test",
-                                            questionnaire_welcome_text="Test", questionnaire_final_text="Test", 
-                                            question_types=[QuestionType.INFO_TEXT])
+            created_questionnaire = self.questionnaire_helper.create_questionnaire_with_questions()
         except Exception as e:
             self.fail(f"Failed to create questionnaire: {e}")
 
@@ -108,39 +106,21 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
         except Exception as e:
             self.fail(f"Failed to navigate to 'Bundles' page: {e}")
 
-        # Assert - Verify that the bundle table is available
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.TABLE_BUNDLE)
-            )
-        except TimeoutException:
-            self.fail(
-                f"Bundle table not found")
+        self.utils.check_visibility_of_element(BundleSelectors.TABLE_BUNDLE, "Bundle table not found")
 
         try:
-            self.bundle_helper.create_bundle(bundle_name, True, [created_questionnaire])
-            self.bundle_helper.save_bundle(bundle_name)
+            bundle = self.bundle_helper.create_bundle(publish_bundle=True, questionnaires=[created_questionnaire])
+            bundle['id']=self.bundle_helper.save_bundle(bundle_name=bundle['name'])
         except Exception as e:
             self.fail(f"Failed to create bundle: {e}")
     
-        #Assert - Verify that flag icons are visible
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.CELL_FLAGICON)
-            )
-        except Exception as e:
-            self.fail(f"Failed to verify flag icon: {e}")
-
-        self.utils.search_item(bundle_name, "bundle")
-        bundle_id = self.bundle_helper.get_bundle_id()
+        self.utils.check_visibility_of_element(BundleSelectors.CELL_FLAGICON, "Flag icon not found")
     
         try:
             self.navigation_helper.navigate_to_manage_clinics()
-            self.clinic_helper.create_clinic(clinic_name=clinic_name, 
-                                             clinic_description="Test Clinic Description",
-                                             configurations=[{'selector': (By.CSS_SELECTOR, '#usePseudonymizationService > div:nth-child(1) > div:nth-child(3) > label:nth-child(1)')}],
-                                             bundles=[{'id': bundle_id, 'name': bundle_name}])
-            clinic_id=self.clinic_helper.save_clinic(clinic_name)
+            clinic["name"] = self.clinic_helper.create_clinic(bundles=[bundle],
+                                                              configurations=[{'selector': (By.CSS_SELECTOR, '#usePseudonymizationService > div:nth-child(1) > div:nth-child(3) > label:nth-child(1)')}])
+            clinic['id']=self.clinic_helper.save_clinic(clinic_name=clinic['name'])
 
         except Exception as e:
             self.fail(f"Failed to create clinic: {e}")
@@ -148,48 +128,30 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
         # Assert - Find clinics assigned to the bundle
         try:
             self.navigation_helper.navigate_to_manage_bundles()
-            self.utils.search_item(bundle_name, "bundle")
+            self.utils.search_item(bundle["name"], "bundle")
             bundle_row = self.bundle_helper.get_first_bundle_row()
             bundle_row.find_element(By.CSS_SELECTOR, "td:nth-child(3)")
             clinic_link = bundle_row.find_element(By.CSS_SELECTOR, "ul > li > a")
-            self.assertEqual(clinic_link.text, clinic_name, f"Clinic name '{clinic_name}' not found in bundle row.")
+            self.assertEqual(clinic_link.text, clinic["name"], f"Clinic name '{clinic["name"]}' not found in bundle row.")
 
         except Exception as e:
             self.fail(f"Failed to find clinic assigned to bundle: {e}")
 
+        self.utils.check_visibility_of_element(BundleSelectors.INPUT_BUNDLE_SEARCH, "Bundle table search box not found")
 
-        #Assert: Verify that search box is available
+        self.utils.check_visibility_of_element(BundleSelectors.PAGINATION_BUNDLE, "Bundle table pagination not found")
+
+        self.utils.check_visibility_of_element(BundleSelectors.BUTTON_ADD_BUNDLE, "Bundle add button not found")
+
         try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.INPUT_BUNDLE_SEARCH)
-            )
-        except TimeoutException:
-            self.fail("Search box not found")
-
-        #Assert: Verify that pagination is available
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.PAGINATION_BUNDLE)
-            )
-        except TimeoutException:
-            self.fail("Pagination not found ")
-
-        #Assert: Verify that add new bundle button is available
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.BUTTON_ADD_BUNDLE)
-            )
-        except TimeoutException:
-            self.fail("Add button not found ")
-
-        #Finally
+            pass
         finally:
-            self.utils.search_and_delete_item(clinic_name,clinic_id, "clinic")
-            self.utils.search_and_delete_item(bundle_name,bundle_id, "bundle")
+            self.utils.search_and_delete_item(clinic["name"],clinic["id"], "clinic")
+            self.utils.search_and_delete_item(bundle["name"],bundle["id"], "bundle")
             self.utils.search_and_delete_item(created_questionnaire['name'], created_questionnaire['id'], "questionnaire")
 
     def test_bundle_fill(self):
-        bundle_name = "Test Bundle"
+        created_questionnaire = {}
         # Arrange
         if not self.secret.get('admin-username') or not self.secret.get('admin-username'):
             self.skipTest("User AD credentials missing. Test skipped.")
@@ -197,10 +159,7 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
         self.authentication_helper.login(self.secret['admin-username'], self.secret['admin-password'])
 
         try:
-            created_questionnaire = self.questionnaire_helper.create_questionnaire_with_questions(questionnaire_name="Test", questionnaire_description="Test",
-                                            questionnaire_language_code="de_DE", questionnaire_display_name="Test",
-                                            questionnaire_welcome_text="Test", questionnaire_final_text="Test", 
-                                            question_types=[QuestionType.INFO_TEXT])
+            created_questionnaire = self.questionnaire_helper.create_questionnaire_with_questions()
         except Exception as e:
             self.fail(f"Failed to create questionnaire: {e}")
 
@@ -210,87 +169,17 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
             self.fail(f"Failed to navigate to 'Bundles' page: {e}")
 
         self.utils.click_element(BundleSelectors.BUTTON_ADD_BUNDLE)
-        
-        #Assert - Visibility of language dropdown
-        try:
-            self.language_helper.open_language_dropdown()
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(LanguageSelectors.LANGUAGE_DROPDOWN)
-            )
-        except Exception as e:  
-            self.fail(f"Failed to open language dropdown: {e}")
-
-        #Assert - Visibility of bundle name input
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.INPUT_NAME)
-            )
-        except Exception as e:  
-            self.fail(f"Failed to locate bundle input: {e}")
-
-        #Assert - Visibility of bundle description input
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.INPUT_EDITABLE_DESCRIPTION)
-            )
-        except Exception as e:  
-            self.fail(f"Failed to locate bundle description: {e}")
-
-        #Assert - Visibility of bundle welcome input
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.INPUT_WELCOME_TEXT)
-            )
-        except Exception as e:  
-            self.fail(f"Failed to locate welcome input: {e}")
-
-        #Assert - Visibility of bundle welcome input
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(BundleSelectors.INPUT_END_TEXT)
-            )
-        except Exception as e:  
-            self.fail(f"Failed to locate end input: {e}")
-        
-        # Assert - Visibility of publish checkbox
-        try:
-            WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BundleSelectors.CHECKBOX_PUBLISH)
-            )
-        except TimeoutException:
-            self.fail("Publish checkbox not found ")
-
-        # Assert - Visibility of name progress checkbox
-        try:
-            WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BundleSelectors.CHECKBOX_NAME_PROGRESS)
-            )
-        except TimeoutException:
-            self.fail("Name progress checkbox not found ")
-
-        # Assert - Visibility of progress whole package checkbox
-        try:
-            WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BundleSelectors.CHECKBOX_PROGRESS_WHOLE_PACKAGE)
-            )
-        except TimeoutException:
-            self.fail("Progress whole package checkbox not found ")
-
-        # Assert - Visibility of available questionnaires table
-        try:
-            WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BundleSelectors.TABLE_AVAILABLE_QUESTIONNAIRES)
-            )
-        except TimeoutException:
-            self.fail("Available questionnaires table not found")
-
-        # Assert - Visibility of assigned questionnaires table
-        try:
-            WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BundleSelectors.TABLE_ASSIGNED_QUESTIONNAIRES)
-            )
-        except TimeoutException:
-            self.fail("Assigned questionnaires table not found")
+        self.language_helper.open_language_dropdown()
+        self.utils.check_visibility_of_element(LanguageSelectors.LANGUAGE_DROPDOWN, "Failed to open language dropdown")
+        self.utils.check_visibility_of_element(BundleSelectors.INPUT_NAME, "Failed to locate bundle input")
+        self.utils.check_visibility_of_element(BundleSelectors.INPUT_EDITABLE_DESCRIPTION, "Failed to locate bundle description input")
+        self.utils.check_visibility_of_element(BundleSelectors.INPUT_WELCOME_TEXT, "Failed to locate welcome input")
+        self.utils.check_visibility_of_element(BundleSelectors.INPUT_END_TEXT, "Failed to locate end input")
+        self.utils.check_visibility_of_element(BundleSelectors.CHECKBOX_PUBLISH, "Failed to locate publish checkbox")
+        self.utils.check_visibility_of_element(BundleSelectors.CHECKBOX_NAME_PROGRESS, "Failed to locate name progress checkbox")
+        self.utils.check_visibility_of_element(BundleSelectors.CHECKBOX_PROGRESS_WHOLE_PACKAGE, "Failed to locate progress whole package checkbox")
+        self.utils.check_visibility_of_element(BundleSelectors.TABLE_AVAILABLE_QUESTIONNAIRES, "Available questionnaires table not found")
+        self.utils.check_visibility_of_element(BundleSelectors.TABLE_ASSIGNED_QUESTIONNAIRES, "Assigned questionnaires table not found")
 
         #Assert - Test for assigning questionnaire to bundle
         try:
@@ -314,21 +203,10 @@ class CustomTest(IMISeleniumChromeTest, unittest.TestCase):
             self.assertEqual(len(validation_errors), 2, "Expected 2 validation errors, but found {len(validation_errors)}")
         except Exception as e:
             self.fail(f"Failed to save bundle: {e}")
-        
-        #Assert - Create a bundle with a questionnaire
-        try:
-            self.navigation_helper.navigate_to_manage_bundles()
-            self.bundle_helper.create_bundle(bundle_name, True, [created_questionnaire])
-            self.bundle_helper.save_bundle(bundle_name)
-        except Exception as e:
-            self.fail(f"Failed to create bundle: {e}")
+    
         
         #Finally
         finally:
-            self.utils.search_item(bundle_name, "bundle")
-            bundle_id = self.bundle_helper.get_bundle_id()
-            if(bundle_id):
-                self.utils.search_and_delete_item(bundle_name,bundle_id, "bundle")
             if(created_questionnaire):
                 self.utils.search_and_delete_item(created_questionnaire['name'], created_questionnaire['id'], "questionnaire")
         
