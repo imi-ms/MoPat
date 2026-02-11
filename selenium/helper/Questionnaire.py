f576283e59f97374d2b10d084fc490134170098c
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2026-02-11 11:32:08 +0100

    Added wait to make test more reliable

--- selenium/helper/Questionnaire.py
+++ selenium/helper/Questionnaire.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 from selenium.common import TimeoutException
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -194,6 +195,7 @@ class QuestionnaireHelper:
 
         # Navigate to "Manage Questionnaires"
         self.navigation_helper.navigate_to_manage_questionnaires()
+        time.sleep(10)
 
         # Create the questionnaire
         self.click_add_questionnaire_button()
