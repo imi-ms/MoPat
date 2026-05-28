16783c5052ab1bad4bb641d25b55bd15f50db390
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2026-05-28 16:46:21 +0200

    Added simple tests to check if flag works on login page

---
+++ src/test/java/de/imi/mopat/config/DemoModeActiveTest.java
@@ -0,0 +1,57 @@
+package de.imi.mopat.config;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+    AppConfig.class,
+    ApplicationSecurityConfig.class,
+    MvcWebApplicationInitializer.class,
+    PersistenceConfig.class
+})
+@TestPropertySource(
+    locations = "classpath:mopat-test.properties",
+    properties = "de.imi.mopat.isDemoInstance=true"
+)
+@WebAppConfiguration
+public class DemoModeActiveTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+            .apply(springSecurity())
+            .build();
+    }
+
+    @Test
+    public void shouldShowDemoLoginValues() throws Exception {
+        mockMvc.perform(get("/mobile/user/login"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("value=\"admin\"")))
+            .andExpect(content().string(containsString("value=\"admin123\"")))
+            .andExpect(content().string(containsString("readonly=\"true\"")));
+    }
+}
